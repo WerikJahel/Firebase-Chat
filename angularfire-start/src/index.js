0d83b53af62457b195e9865b304140313c8d8252
Werik Jahel <werikjahel@gmail.com>
2023-11-13 17:39:53 -0300

    Tentativa de inicialização

---
+++ angularfire-start/src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCSBcmUEqPYIV2aMgJ0kSN1JTkzYLyB2kg",
+    authDomain: "fir-chat-67bb3.firebaseapp.com",
+    projectId: "fir-chat-67bb3",
+    storageBucket: "fir-chat-67bb3.appspot.com",
+    messagingSenderId: "489496812986",
+    appId: "1:489496812986:web:8ea8788cb38eaeed085183"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
